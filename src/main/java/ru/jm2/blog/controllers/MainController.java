4b463bbe59738a6e67408af3fb6916c64d52c8b8
Dmitry <nikitindvi@gmail.com>
2021-07-13 11:24:54 +0300

    3й урок

--- src/main/java/ru/jm2/blog/controllers/MainController.java
+++ src/main/java/ru/jm2/blog/controllers/MainController.java
@@ -15,8 +15,8 @@ public class MainController {
 
     @GetMapping("/about")
     public String about(Model model) {
-        model.addAttribute("title", "Главная страница");
-        return "home";
+        model.addAttribute("title", "Страница про нас");
+        return "about";
     }
 
 }
